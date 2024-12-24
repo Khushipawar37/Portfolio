d97a5a61c103d380ca49476a3f7ade3d6af402d2
Khushipawar37 <khushipawar987@gmail.com>
2024-12-24 22:02:07 +0530

    Finished working on the navbar

--- src/Navbar/Navbar.jsx
+++ src/Navbar/Navbar.jsx
@@ -1,9 +1,17 @@
-import './navbar.css'
+import "./navbar.css";
 
-export default function Navbar (){
-    return (
-        <>
+export default function Navbar() {
+  return (
+    <>
+      <div className="nav">
         <div className="navHead">Khushi</div>
-        </>
-    )
+        <div className="navIcons">
+          <div className="navAbout">About</div>
+          <div className="navSkills">Skills</div>
+          <div className="navProj">Projects</div>
+          <div className="navCon">Contact</div>
+        </div>
+      </div>
+    </>
+  );
 }
