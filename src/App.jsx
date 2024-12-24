28603567f5f882a33b8f9198c72ce2125be2e662
Khushipawar37 <khushipawar987@gmail.com>
2024-12-25 01:17:36 +0530

    Started with the landing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
 import './App.css'
 import Navbar from './Navbar/Navbar'
+import Landing from './Landing/Landing'
 
 function App() {
 
   return (
     <>
     <Navbar/>
+    <Landing/>
     </>
   )
 }
---
+++ src/Landing/Landing.jsx
@@ -0,0 +1,12 @@
+import './landing.css'
+
+export default function Landing(){
+
+    return(
+        <>
+        <div className="main">KHUSHI PAWAR</div>
+        <div className="role">Full Stack Developer</div>
+        <div className="byLine">Simplifying complexity through thoughtful <br/>design and meticulous coding. Building seamless <br/>bridges between innovative ideas and impactful solutions.</div>
+        </>
+    )
+}
