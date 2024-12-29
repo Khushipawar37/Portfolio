4015f0102613e7b70d97933e6899a3fd5c7fa5c7
Khushipawar37 <khushipawar987@gmail.com>
2024-12-30 00:17:49 +0530

    Added the content for landing page

--- src/Landing/Landing.jsx
+++ src/Landing/Landing.jsx
@@ -1,12 +1,23 @@
-import './landing.css'
+import "./landing.css";
+import dp from "/src/assets/dpp.jpg";
 
-export default function Landing(){
-
-    return(
-        <>
-        <div className="main">KHUSHI PAWAR</div>
-        <div className="role">Full Stack Developer</div>
-        <div className="byLine">Simplifying complexity through thoughtful <br/>design and meticulous coding. Building seamless <br/>bridges between innovative ideas and impactful solutions.</div>
-        </>
-    )
+export default function Landing() {
+  return (
+    <>
+      <div className="intro">
+        <div className="content">
+          <div className="main">KHUSHI PAWAR</div>
+          <div className="role">Full Stack Developer</div>
+          <div className="byLine">
+            Simplifying complexity through thoughtful <br />
+            design and meticulous coding. Building seamless <br />
+            bridges between innovative ideas and impactful solutions.
+          </div>
+        </div>
+        <div className="mainImg">
+          <img src={dp}></img>
+        </div>
+      </div>
+    </>
+  );
 }
