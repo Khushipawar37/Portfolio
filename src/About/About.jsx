d87c4323b08d1946124dc20477dac3fa7f9edeb5
Khushipawar37 <khushipawar987@gmail.com>
2025-01-01 01:16:41 +0530

    Working on the about section

--- src/About/About.jsx
+++ src/About/About.jsx
@@ -4,6 +4,7 @@ export default function About(){
 
     return(
         <>
+        <div className="aboutHead">About</div>
         </>
     )
 }
--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import './App.css'
 import Navbar from './Navbar/Navbar'
 import Landing from './Landing/Landing'
+import About from './About/About'
 
 function App() {
 
@@ -9,6 +10,7 @@ function App() {
     <>
     <Navbar/>
     <Landing/>
+    <About/>
     </>
   )
 }
