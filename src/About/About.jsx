e4090aa0cb900560fe6e26486f1db19b0fbc8863
Khushipawar37 <khushipawar987@gmail.com>
2025-01-02 00:48:43 +0530

    Added avator for the about section

--- src/About/About.jsx
+++ src/About/About.jsx
@@ -1,10 +1,14 @@
 import './about.css'
+import abt from "/src/assets/abt.png";
 
 export default function About(){
 
     return(
         <>
         <div className="aboutHead">About</div>
+        <div className="aboutImg">
+            <img src={abt}></img>
+        </div>
         </>
     )
 }
